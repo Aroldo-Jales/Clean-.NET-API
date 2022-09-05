67fce725df53a671b2d51a64ecdfbfb598d4aba8
Aroldo Jales <aroldo_jales@hotmail.com>
2022-09-05 16:42:00 -0300

    fix(Infrastructure): use AppDirectories.getDatabasePath in AppDbContext

--- Prova1.Infrastructure/Database/AppDbContext.cs
+++ Prova1.Infrastructure/Database/AppDbContext.cs
@@ -20,11 +20,11 @@ namespace Prova1.Infrastructure.Database
         {       
             // FOR APPLICATION RUNNING USE AppDirectories.getDatabasePath
             //
-            // optionsBuilder.UseSqlite(connectionString: String.Format(@"DataSource={0}; Cache=Shared", AppDirectories.getDatabasePath));
+            optionsBuilder.UseSqlite(connectionString: String.Format(@"DataSource={0}; Cache=Shared", AppDirectories.getDatabasePath));
 
             // FOR MIGRATIONS AND DATABASE UPDATES USE ABSOLUTE PATH
             //
-            optionsBuilder.UseSqlite(connectionString: String.Format(@"DataSource={0}; Cache=Shared", @"C:\Users\Aroldo Jales\Documents\Code\Vscode\IFPI\PROVA1\Prova1\Prova1.Infrastructure\Database\SQLite\AppDatabase.db"));            
+            //optionsBuilder.UseSqlite(connectionString: String.Format(@"DataSource={0}; Cache=Shared", @"C:\Users\Aroldo Jales\Documents\Code\Vscode\IFPI\PROVA1\Prova1\Prova1.Infrastructure\Database\SQLite\AppDatabase.db"));            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
